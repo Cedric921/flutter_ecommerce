51539ecf15ffa929ac94ca4c1f815d59184b8dbc
cedric karungu <ckarungu921@gmail.com>
2022-03-17 16:45:21 +0200

    add the body

--- lib/second_widget.dart
+++ lib/second_widget.dart
@@ -8,11 +8,32 @@ class SecondWidget extends StatefulWidget {
 }
 
 class _SecondWidgetState extends State<SecondWidget> {
+  int increment = 0;
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
       appBar: AppBar(
         title: const Text("c-store"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text("Vous venez de cliquer "),
+            const SizedBox(
+              height: 10,
+            ),
+            Text("$increment",
+             style:  const TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
